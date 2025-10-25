f41db64175357790fd8956af16b60886e9368854
Ben Walker <b6nw4r@gmail.com>
2025-10-25 19:29:52 -0400

    Add more react eslint plugins

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -2,6 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 import stylistic from "@stylistic/eslint-plugin";
@@ -16,6 +18,8 @@ export default defineConfig([
       tseslint.configs.stylisticTypeChecked,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
       stylistic.configs.customize({
         commaDangle: "always-multiline",
         indent: 2,
