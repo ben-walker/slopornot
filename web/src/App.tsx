5ff266538b98082da1e7fe2dc825f9a90e759fc3
Ben Walker <b6nw4r@gmail.com>
2025-10-26 22:02:17 -0400

    Setup mantine

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { MantineProvider } from "@mantine/core"; // TODO: non-feature App component with providers
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "@mantine/core/styles.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <MantineProvider>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -25,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </MantineProvider>
   );
 }
 
