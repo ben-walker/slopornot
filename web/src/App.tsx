e22dd114094ea1aba5430694b109128613263565
Ben Walker <b6nw4r@gmail.com>
2025-10-26 22:22:40 -0400

    Use mantine components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,20 @@
 import "@mantine/core/styles.css";
 import { useState } from "react";
-import { MantineProvider } from "@mantine/core"; // TODO: non-feature App component with providers
+import { MantineProvider, Button, Title } from "@mantine/core"; // TODO: non-feature App component with providers
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleClick = () => {
+    setCount(count => count + 1);
+  };
+
   return (
     <MantineProvider>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => { setCount(count => count + 1); }}>
-          {`count is ${count.toString()}`}
-        </button>
-      </div>
+      <Title order={3}>Vite + React</Title>
+      <Button onClick={handleClick}>
+        {`count is ${count.toString()}`}
+      </Button>
     </MantineProvider>
   );
 }
