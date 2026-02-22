798446b0e6884787a87adede58adb727ddc1eafe
Ben Walker <b6nw4r@gmail.com>
2026-02-21 22:35:19 -0500

    helmet, graceful shutdown

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,12 @@
 import Fastify from "fastify";
+import helmet from "@fastify/helmet";
 
 const buildApp = () => {
   const app = Fastify({
     logger: true,
   });
+
+  app.register(helmet);
 
   return app;
 };
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,26 @@
 import { buildApp } from "./app";
+import closeWithGrace from "close-with-grace";
 
 const app = buildApp();
+
+closeWithGrace(async ({ err, signal }) => {
+  if (err) {
+    app.log.error({ err }, "Server closing with error");
+  }
+  else if (signal) {
+    app.log.info(`${signal} received, server closing`);
+  }
+  else {
+    app.log.info("Server closing due to timeout");
+  }
+
+  await app.close();
+});
 
 try {
   await app.listen({ port: 3000 }); // TODO: env configured
 }
 catch (err) {
-  app.log.error(err);
+  app.log.error({ err }, "Server failed to start");
   process.exit(1);
 }
