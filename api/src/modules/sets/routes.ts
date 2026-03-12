0e2d00798d10e0132acb04786c891673c9dad684
Ben Walker <b6nw4r@gmail.com>
2026-03-12 00:48:24 -0400

    add initial GET to return image set

--- api/src/modules/sets/routes.ts
+++ api/src/modules/sets/routes.ts
@@ -1,8 +1,32 @@
 import type { FastifyPluginCallbackTypebox } from "@fastify/type-provider-typebox";
+import { Type } from "@fastify/type-provider-typebox";
 
 const sets: FastifyPluginCallbackTypebox = (app) => {
-  app.get("/", () => {
-    return { message: "hello world" };
+  app.get("/:date", {
+    schema: {
+      params: Type.Object({
+        date: Type.String({ format: "date" }),
+      }),
+    },
+  }, async (request, reply) => {
+    const { date } = request.params;
+
+    const set = await app.db.query.sets.findFirst({
+      where: (sets, { eq }) => eq(sets.date, date),
+      with: {
+        images: {
+          columns: {
+            is_ai: false, // Excluded from response to prevent cheating
+          },
+        },
+      },
+    });
+
+    if (!set) {
+      return reply.notFound();
+    }
+
+    return set;
   });
 };
 
