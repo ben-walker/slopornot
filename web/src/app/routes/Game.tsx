d36f07cafa0c2ff56fe8211b6b7ce54c34b2c782
Ben Walker <b6nw4r@gmail.com>
2026-03-17 20:59:24 -0400

    add date-fns to web, make api call

--- web/src/app/routes/Game.tsx
+++ web/src/app/routes/Game.tsx
@@ -1,4 +1,13 @@
+import { format } from "date-fns";
+import { useGetSetsDate } from "src/api/generated";
+
 function Game() {
+  const today = format(new Date(), "yyyy-MM-dd");
+
+  const { data } = useGetSetsDate(today);
+
+  console.log(today, data);
+
   return <div />;
 }
 
