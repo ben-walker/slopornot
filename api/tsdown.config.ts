d1aa314910df594686e897f0d1eb693ef49820b6
Ben Walker <b6nw4r@gmail.com>
2026-06-23 00:40:49 -0400

    api version bumps

--- api/tsdown.config.ts
+++ api/tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   deps: {
-    onlyAllowBundle: false,
+    onlyBundle: false,
   },
   entry: {
     "main": "./src/main.ts",
